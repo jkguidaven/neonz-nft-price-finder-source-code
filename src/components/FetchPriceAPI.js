7d3ef10da1b3d3dcac5fb2a9901a71db3273248a
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-10-28 14:55:56 +0800

    fetch price correctly

---
+++ src/components/FetchPriceAPI.js
@@ -0,0 +1,257 @@
+export default (start, end) => {
+  return fetch("https://api.objkt.com/v1/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      operationName: "getObjktDetailed",
+      query: `
+            query getObjktDetailed($fa2: String!) {
+                token(where: {fa2_id: {_eq: $fa2}}) {
+                    ...TokenDefault
+                    ...TokenMarket
+                    ...TokenHolders
+                    __typename
+                }
+                }
+
+                fragment TokenDefault on token {
+                id
+                artifact_uri
+                creator_id
+                description
+                display_uri
+                thumbnail_uri
+                fa2_id
+                royalties
+                supply
+                timestamp
+                title
+                mime
+                last_listed
+                highest_bid
+                lowest_ask
+                creator {
+                    ...UserDefault
+                    __typename
+                }
+                token_attributes {
+                    attribute {
+                    name
+                    type
+                    value
+                    __typename
+                    }
+                    __typename
+                }
+                __typename
+                }
+
+                fragment UserDefault on holder {
+                address
+                alias
+                site
+                twitter
+                description
+                tzdomain
+                __typename
+                }
+
+                fragment TokenMarket on token {
+                english_auctions(where: {status: {_eq: "active"}}) {
+                    ...EnglishAuctionDefault
+                    __typename
+                }
+                dutch_auctions(where: {status: {_eq: "active"}}) {
+                    ...DutchAuctionDefault
+                    __typename
+                }
+                bids(order_by: {price: desc}, where: {status: {_eq: "active"}}) {
+                    ...BidDefault
+                    __typename
+                }
+                asks(order_by: {price: asc}, where: {status: {_eq: "active"}}) {
+                    ...AskDefault
+                    __typename
+                }
+                swaps(
+                    order_by: {price: asc}
+                    where: {status: {_eq: "active"}, contract_version: {_gt: 1}, is_valid: {_eq: true}}
+                ) {
+                    ...SwapDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment EnglishAuctionDefault on obj_english_auction {
+                id
+                hash
+                fa2_id
+                price_increment
+                reserve
+                royalties
+                start_time
+                status
+                end_time
+                timestamp
+                token_id
+                update_level
+                update_timestamp
+                hash
+                artist {
+                    ...UserDefault
+                    __typename
+                }
+                bids {
+                    ...EnglishAuctionBidsDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment EnglishAuctionBidsDefault on obj_english_bid {
+                amount
+                bidder {
+                    ...UserDefault
+                    __typename
+                }
+                bidder_id
+                id
+                timestamp
+                __typename
+                }
+
+                fragment DutchAuctionDefault on obj_dutch_auction {
+                id
+                hash
+                fa2_id
+                buy_price
+                start_price
+                end_price
+                end_time
+                royalties
+                start_time
+                status
+                timestamp
+                token_id
+                update_level
+                update_timestamp
+                hash
+                artist {
+                    ...UserDefault
+                    __typename
+                }
+                buyer {
+                    ...UserDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment BidDefault on obj_bid {
+                id
+                price
+                royalties
+                status
+                timestamp
+                token_id
+                update_timestamp
+                fa2_id
+                artist {
+                    ...UserDefault
+                    __typename
+                }
+                creator {
+                    ...UserDefault
+                    __typename
+                }
+                seller {
+                    ...UserDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment AskDefault on obj_ask {
+                id
+                amount
+                amount_left
+                price
+                royalties
+                status
+                timestamp
+                token_id
+                update_timestamp
+                fa2_id
+                artist {
+                    ...UserDefault
+                    __typename
+                }
+                creator {
+                    ...UserDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment SwapDefault on hen_swap {
+                id
+                amount
+                amount_left
+                price
+                royalties
+                status
+                timestamp
+                token_id
+                creator {
+                    ...UserDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment TokenHolders on token {
+                token_holders(where: {quantity: {_gt: "0"}}) {
+                    ...TokenHolderDefault
+                    __typename
+                }
+                __typename
+                }
+
+                fragment TokenHolderDefault on token_holder {
+                id
+                holder_id
+                quantity
+                token_id
+                holder {
+                    ...UserDefault
+                    __typename
+                }
+                __typename
+                }
+      `,
+      variables: {
+        fa2: "KT1MsdyBSAMQwzvDH4jt2mxUKJvBSWZuPoRJ",
+      },
+    }),
+  })
+    .then((r) => r.json())
+    .then(({ data: { token } }) => {
+      const finalData = token
+        .map((neonz) => {
+          console.log(neonz);
+          return {
+            id: Number(neonz.id),
+            price: (neonz.asks.length ? neonz.asks[0].price : 0) ?? neonz.highest_bid,
+          };
+        })
+        .filter((neonz) => {
+          return neonz.id >= start && neonz.id <= end && neonz.price;
+        })
+        .sort((a, b) => a.price - b.price);
+      return finalData;
+    });
+};
--- src/components/NeonzList.vue
+++ src/components/NeonzList.vue
@@ -1,77 +1,99 @@
 <template>
   <div class="container">
     <div
-        class="neonz"
-        v-for="(neonz,i) in items"
-        :key="`neonz-${i}`"
-        @click="redirect(`https://objkt.com/asset/neonz/${neonz.id}`)"
+      class="neonz"
+      v-for="(neonz, i) in data"
+      :key="`neonz-${i}`"
+      @click="redirect(`https://objkt.com/asset/neonz/${neonz.id}`)"
     >
-        <img :src="neonz.image" />
+      <img :src="neonz.image" />
 
-        <div class="info-box">
-            <h1> NEONZ# {{ neonz.id}}</h1>
-            <h4> Rarity Rank: {{ neonz.rank}}</h4>
-        </div>
+      <div class="info-box">
+        <h1>NEONZ# {{ neonz.id }}</h1>
+        <h4>Rarity Rank: {{ neonz.rank }}</h4>
+        <h4>Price: {{ neonz.price / 1000000 }} tez</h4>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import neonzs from '../assets/neonz-metadata.json';
+import neonzs from "../assets/neonz-metadata.json";
+import { watchEffect, ref } from "vue";
+import fetchPriceAPI from "./FetchPriceAPI";
 
 export default {
-  name: 'NeonList',
+  name: "NeonList",
 
   props: {
     page: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
 
     size: {
-        type: Number,
-        default: 12
-    }
+      type: Number,
+      default: 20,
+    },
+
+    rankStart: {
+      type: Number,
+      default: 1,
+    },
+
+    rankEnd: {
+      type: Number,
+      default: 10000,
+    },
   },
 
-  setup() {
-      return {
-        neonzs
-      };
+  setup(prop) {
+    const data = ref([]);
+    const getMeta = (id) => {
+      return neonzs.find((neonz) => neonz.id === id);
+    };
+    watchEffect(async () => {
+      const result = await fetchPriceAPI(prop.rankStart, prop.rankEnd);
+      data.value = result.slice(prop.page * prop.size, prop.page * prop.size + prop.size).map((neonz) => ({
+        ...getMeta(neonz.id),
+        price: neonz.price,
+      }));
+    });
+
+    return {
+      neonzs,
+      data,
+    };
   },
 
-  computed: {
-    items() {
-        return this.neonzs.slice(this.page * this.size, this.page * this.size + this.size);
-    }
-  },
+  computed: {},
 
   methods: {
     redirect(url) {
-        window.open(url);
-    }
-  }
-}
+      window.open(url);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .container {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 }
 
 .neonz {
-    border: 1px solid #2529ff;
-    cursor: pointer;
+  border: 1px solid #2529ff;
+  cursor: pointer;
 }
 
 .neonz:hover {
-    background: #2529ff;
-    color: white;
+  background: #2529ff;
+  color: white;
 }
 
 img {
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 }
 </style>
